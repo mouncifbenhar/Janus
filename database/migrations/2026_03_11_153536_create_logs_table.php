bb35dc87a401a7479810969129150e54956f35c4
monsef benhar <mouncifbenhar7@gmail.com>
2026-03-13 09:54:27 +0000

    logs migration

---
+++ database/migrations/2026_03_11_153536_create_logs_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('habit_id')->constrained('habits')->cascadeOnDelete();
+            $table->date('complet_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('logs');
+    }
+};
